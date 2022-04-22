bc895b20d2557d17b4500bcb1e77b5a8653e9ec3
Rinat <hellemployee666@gmail.com>
2022-04-22 23:04:44 +0300

    [Added] Config

---
+++ _Scripts/Config.py
@@ -0,0 +1,15 @@
+NAME = "NeCalculator "
+
+# TypeName
+COMMON = ": COMMON"
+SCIENTIFIC = ": SCIENTIFIC"
+PROGRAMMER = ": PROGRAMMER"
+PLOTTING = ": PLOTTING"
+
+ICON_PATH = '../Resources/calculator.ico'
+BACKGROUND_COLOR = "#171717"
+BORDER_WIDTH = 10
+
+SCIENTIFIC_BTN_PARAMS = {'bd':5, 'fg':'#BBB', 'bg':'#3C3636', 'font':('sans-serif', 20, 'bold')}
+NUMBER_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#BBB', 'font':('sans-serif', 20, 'bold')}
+DELETE_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#db701f', 'font':('sans-serif', 20, 'bold')}
