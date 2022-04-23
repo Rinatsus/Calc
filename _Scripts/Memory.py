b8460642daf1220344e894f29148777858ffbaa8
Rinat <hellemployee666@gmail.com>
2022-04-23 22:06:40 +0300

    [Added] Save system to Memory

--- _Scripts/Memory.py
+++ _Scripts/Memory.py
@@ -1,21 +1,23 @@
 import tkinter as tk
 from Config import *
-fro
+from Constans import *
+import os.path
 
 
 class Cell(tk.Frame):
-    def __init__(self, frame: tk.Frame, text: str):
+    def __init__(self, frame: tk.Frame, val: str, name):
         super().__init__(frame, bg='#b0afae')
+        self.name = name
         self.parent = frame
         self.selected = tk.IntVar()
         self.text_result = tk.StringVar()
-
-        self.result = 0
-        self.text_result.set(0)
+        print(val)
+        self.result = val
+        self.text_result.set(self.result)
 
         self.label = tk.Label(self, anchor='w', width=10, bg='#b0afae', textvariable=self.text_result,
-                                           font=('sans-serif', 15, 'bold')
-                                           ).pack(side='left')
+                              font=('sans-serif', 15, 'bold')
+                              ).pack(side='left')
         tk.Checkbutton(self, variable=self.selected).pack(side='left')
 
     def set(self, val):
@@ -37,19 +39,59 @@ class Memory(tk.Frame):
         self.parent = window
         self.memory_cells = []
         self.grid(row=0, column=10, rowspan=10, sticky='n')
+        self.data = Data()
+        self.load()
 
-    def add_cell(self, text: str):
+    def add_cell(self, val):
         if len(self.memory_cells) < MAX_MEMORY_CELLS:
-            cell = Cell(self, text)
+            cell = Cell(self, get_int_or_float(val), len(self.memory_cells))
             cell.grid(pady=10, sticky="e")
             self.memory_cells.append(cell)
+            self.data.append(cell.name, cell.result)
+            self.save()
 
     def add_result(self, val):
         for cell in self.memory_cells:
             if bool(cell.selected.get()):
                 cell.add(val)
+                self.data.append(cell.name, cell.result)
+        self.save()
 
     def substrate_result(self, val):
         for cell in self.memory_cells:
             if bool(cell.selected.get()):
                 cell.substrate(val)
+                self.data.append(cell.name, cell.result)
+        self.save()
+
+    def save(self):
+        with open(DATA_PATH, 'w') as file:
+            file.write("{")
+            for key, val in self.data.results.items():
+                file.write('{}:{},'.format(key, val))
+            file.write("}")
+
+    def load(self):
+        if os.path.exists(DATA_PATH) == False:
+            return
+
+        with open(DATA_PATH, 'r') as file:
+            try:
+                self.data.results = eval(file.readline())
+            except Exception:
+                return
+
+            for name, result in self.data.results.items():
+                self.add_cell(result)
+
+
+
+class Data:
+    def __init__(self):
+        self.results = {}
+
+    def set(self, results):
+        self.results = results
+
+    def append(self, name, result):
+        self.results[name] = result
