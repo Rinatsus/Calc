a33b8314523cfbea17eda5e96bbf99c11ee9b9b7
Rinat <hellemployee666@gmail.com>
2022-04-25 01:03:32 +0300

    [Edit] added chars and btn params

--- _Scripts/Config.py
+++ _Scripts/Config.py
@@ -1,4 +1,3 @@
-
 NAME = "NeCalculator "
 
 # TypeName
@@ -12,8 +11,22 @@ NAMES = [
     PROGRAMMER,
     PLOTTING
 ]
-
+# Signs
 PI = '\u03C0'
+DOT = '.'
+PLUS = '+'
+MINUS = '-'
+SUB = '/'
+MUL = "*"
+POW = '**'
+
+CHARS = [
+    PLUS,
+    MINUS,
+    SUB,
+    MUL,
+    POW
+]
 
 GEOMETRY = '400x500'
 
@@ -21,16 +34,15 @@ ICON_PATH = '../Resources/calculator.ico'
 BACKGROUND_COLOR = "#171717"
 BORDER_WIDTH = 10
 
-SCIENTIFIC_BTN_PARAMS = {'bd':5, 'fg':'#BBB', 'bg':'#3C3636', 'font':('sans-serif', 20, 'bold')}
-NUMBER_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#BBB', 'font':('sans-serif', 20, 'bold')}
-DELETE_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#db701f', 'font':('sans-serif', 20, 'bold')}
-MEMORY_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#db701f', 'font':('sans-serif', 20, 'bold')}
-CELLS_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#db701f', 'font':('sans-serif', 15, 'bold')}
+SCIENTIFIC_BTN_PARAMS = {'bd': 5, 'fg': '#BBB', 'bg': '#3C3636', 'font': ('sans-serif', 20, 'bold')}
+NUMBER_BTN_PARAMS = {'bd': 5, 'fg': '#000', 'bg': '#BBB', 'font': ('sans-serif', 20, 'bold')}
+DELETE_BTN_PARAMS = {'bd': 5, 'fg': '#000', 'bg': '#db701f', 'font': ('sans-serif', 20, 'bold')}
+MEMORY_BTN_PARAMS = {'bd': 5, 'fg': '#000', 'bg': '#db701f', 'font': ('sans-serif', 20, 'bold')}
+CELLS_BTN_PARAMS = {'bd': 5, 'fg': '#000', 'bg': '#db701f', 'font': ('sans-serif', 15, 'bold')}
+PLOTTING_BTN_PARAMS = {'bd': 5, 'fg': '#000', 'bg': '#48e520', 'font': ('sans-serif', 15, 'bold')}
+CLEAR_BTN_PARAMS = {'bd': 5, 'fg': '#000', 'bg': '#db701f', 'font': ('sans-serif', 15, 'bold')}
 
 MAX_MEMORY_CELLS = 4
-MEMORY_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#db701f', 'font':('sans-serif', 20, 'bold')}
-CELLS_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#db701f', 'font':('sans-serif', 15, 'bold')}
-PLOTTING_BTN_PARAMS = {'bd':5, 'fg':'#000', 'bg':'#48e520', 'font':('sans-serif', 15, 'bold')}
-MAX_MEMORY_CELLS = 4
 
-DATA_PATH = '../Resources/Saved/Data.txt'
+SCIENTIFIC_DATA_PATH = '../Resources/Saved/Data.txt'
+PLOT_DATA_PATH = '../Resources/Saved/Plot.txt'
